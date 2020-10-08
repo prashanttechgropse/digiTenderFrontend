ce7cb7287348403f936678759275594f0ffd7fc1
kansal781 <kansal.781@gmail.com>
2020-10-08 18:24:29 +0530

    fsabfaskg

---
+++ build/precache-manifest.7cd1a5c6839ae43fe3c717545d6691c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686113cbc10203969f519b630b0e8b35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e519e2f579cafbaa38d",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "55df8a0a0614b2f04403",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e519e2f579cafbaa38d",
+    "url": "/static/js/2.d9aeefb0.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/static/js/2.d9aeefb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55df8a0a0614b2f04403",
+    "url": "/static/js/main.bb81df09.chunk.js"
+  },
+  {
+    "revision": "8013b9a6bfb0ea21c879",
+    "url": "/static/js/runtime-main.fdb12973.js"
+  }
+]);
